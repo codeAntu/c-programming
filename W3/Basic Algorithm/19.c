0622946370a882648bbf2761180a616f73101830
Ananta Karmakar <codeantu@gmail.com>
2022-07-11 10:28:45 +0530

    Create 19.c

---
+++ W3/Basic Algorithm/19.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a non negetive number -> ");
+    scanf("%d",&a);
+    if(a<0)
+        printf("Number is negetive.");
+    else if(a%13==0)
+        printf("Number is multiple of 13.");
+    else if(a%13==1)
+        printf("Number is one more than a multiple of 13.");
+     else
+        printf("😐")
+       return 0;
+} 
