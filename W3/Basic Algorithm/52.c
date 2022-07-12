4b1a0db667f8ed234b46c157dc72b9efe34f7eaf
Ananta Karmakar <codeantu@gmail.com>
2022-07-12 19:09:33 +0530

    Create 52.c

---
+++ W3/Basic Algorithm/52.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int *arr;
+    int i,n,sum=0;
+
+    printf("Length of the array -> ");
+    scanf("%d",&n);
+
+    arr=(int *)malloc(n*sizeof(int));
+    for(i=0; i<n; i++) {
+        printf("Enter number -> ");
+        scanf("%d",(arr+i));
+    }
+    for(i=0; i<n; i++) {
+        if(arr[i]==17)
+            arr[i]=0;
+        sum=sum+arr[i];
+    }
+    printf("Sum = %d",sum);
+
+    return 0;
+}
