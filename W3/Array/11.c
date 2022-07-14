7b9e6720c76a1aa5c77e0f25d59be8a77af9fe4f
Ananta Karmakar <codeantu@gmail.com>
2022-07-14 11:47:08 +0530

    Create 11.c

---
+++ W3/Array/11.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+    int *arr;
+    int n,i,j,t,sort;
+
+    printf("Input the number of elements to store in the array :");
+    scanf("%d",&n);
+    arr=(int *)malloc(n*sizeof(int));
+    for(i=0; i<n; i++) {
+        printf("Elements- %d -> ",i);
+        scanf("%d",&arr[i]);
+    }
+    for(i=0; i<n-1; i++) {
+        sort = 1;
+        for(j=0; j<n-1; j++) {
+            if(arr[j]>arr[j+1]) {
+                t=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=t;
+                sort=0;
+            }
+        }
+        if(sort) break;
+    }
+    for(i=0; i<n; i++) {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
