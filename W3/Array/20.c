554cf9eec2ef83c890a021ecbed58a9425ac6485
Ananta Karmakar <codeantu@gmail.com>
2022-07-14 23:37:13 +0530

    Create 20.c

---
+++ W3/Array/20.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main()
+{
+    int arr[3][3],brr[3][3],new[3][3];
+    int i,j;
+    printf("Arr on 1\n");
+    for(i=0; i<3; i++) {
+        for(j=0; j<3; j++) {
+            printf("element [%d][%d] :",i,j);
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    printf("Arr no 2\n");
+    for(i=0; i<3; i++) {
+        for(j=0; j<3; j++) {
+            printf("element [%d][%d] :",i,j);
+            scanf("%d",&brr[i][j]);
+        }
+    }
+    for(i=0; i<3; i++) {
+        for(j=0; j<3; j++) {
+            new[i][j]=arr[i][j]-brr[i][j];
+        }
+    }
+    for(i=0; i<3; i++) {
+        for(j=0; j<3; j++) {
+            printf("%d ",new[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
